92cb66b57330b9970fe6de381e1999cc92538ddd
devikavijayaraghavan <devikavijay@gmail.com>
2026-02-17 12:50:15 -0600

    Started about me section and started working on displaying contact information

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,7 +4,9 @@ import { Info } from './Info.jsx';
 
 export const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
+    <h1>Devika Vijayaraghavan</h1>
+    <h2>About me:</h2>
+	<p>Hello! I'm Devika. I'm a Junior at UW-Madison pursuing a Data Science BS and Computer Science Certificate. My interests include...</p>
     <Hello/>
     <Info/>
   </div>

7cdfca1d3b36a326c15d838987a5a5a53acd43b9
devikavijayaraghavan <devikavijay@gmail.com>
2026-02-19 12:52:51 -0600

    Finalized page layout

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
+import { Contact } from "./Contact.jsx"
 
 export const App = () => (
   <div>
     <h1>Devika Vijayaraghavan</h1>
-    <h2>About me:</h2>
-	<p>Hello! I'm Devika. I'm a Junior at UW-Madison pursuing a Data Science BS and Computer Science Certificate. My interests include...</p>
+    <div className = "boxLayout">
+      <div className = "header">
+        <h2>About me:</h2>
+      </div>
+        <div className = "paragraph">
+	  <p>Hello! I'm Devika. I'm a Junior at UW-Madison pursuing a Data Science BS and Computer Science Certificate. My interests include...</p>
+        </div>
+    </div>
     <Hello/>
     <Info/>
   </div>
